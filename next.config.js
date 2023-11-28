aeb45be437684be1271943b67ac07b9b6bac9d4a
Rof1yev <rofiyevdilshod@gmail.com>
2023-11-28 22:58:57 +0500

    quiz-app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["www.google.com", "uwswpa.org", "icons.veryicon.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "@/theme";
+import NextNProgress from "nextjs-progressbar";
+import "react-multi-carousel/lib/styles.css";
+import { StateContext } from "@/context";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <NextNProgress
+        color="#3182ce"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <StateContext>
+        <Component {...pageProps} />
+      </StateContext>
+    </ChakraProvider>
+  );
 }
